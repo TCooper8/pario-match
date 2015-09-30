4fc686c2dce1e0a049a9cb238329b416beeb9697
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2015-09-30 13:24:02 -0600

    Small update

--- test/match.js
+++ test/match.js
@@ -3,7 +3,6 @@
 let Assert = require('assert')
 let match = require('../lib')
 let option = require('pario-monad').option
-let Pato = require('pato')
 let _ = require('lodash')
 
 let timed = iters => f => {
@@ -26,14 +25,7 @@ describe('Should test matching', () => {
       .else( 5 )
       .bind
 
-    let g = value => Pato.match(value)
-      .type('string').then( 'hello' )
-      .type('number').then( 5 )
-      .done()
 
-    Assert.equal(
-      timed(1000000)( () => f(5) ),
-      timed(1)( () => g(5) )
-    )
+    timed(100)( () => f(5) )
   })
 })

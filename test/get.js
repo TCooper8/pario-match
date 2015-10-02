be5e755b389530b4fdd407b6cddb46dc87399371
tcooper8 <coopetyl@live.com>
2015-10-01 23:58:14 -0600

    Updated Match::get method to match object schemas

--- test/get.js
+++ test/get.js
@@ -7,17 +7,23 @@ let _ = require('lodash')
 describe('Testing match for multiple keys', () => {
   it('should extract the values from the keys', () => {
     Match()
-      .get([ 'name', 'id' ])( _.spread( (name, id) => {
+      .get('name', 'id')( (name, id) => {
         Assert.equal(name, 'bob')
         Assert.equal(id,  7)
-      }))
-      //.get([ 'name', 'id' ])( values => {
-      //  Assert.equal(values[0], 'bob')
-      //  Assert.equal(values[1], 7)
-      //})
+      })
       .bind({
         name: 'bob',
         id: 7
       })
+
+    Match()
+    .get({ name: String, id: 7 })( (name, id) => {
+      Assert.equal(name, 'bob')
+      Assert.equal(id, 7)
+    })
+    .bind({
+      name: 'bob',
+      id: 7
+    })
   })
 })
